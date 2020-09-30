80458f873c39635f7ad8bb1ff7be51116147c6b0
Flavio Oria <flavio.oriap@gmail.com>
2020-09-30 17:42:16 +0200

    se añadio un media query en js

--- js/main.js
+++ js/main.js
@@ -2,11 +2,20 @@ const menu = document.querySelector('.menu')
 const burgerButton = document.querySelector('#burger-menu')
 const ipadResponse= window.matchMedia('screen and (max-width:767px')
 
-/*metodo mas corto*/ 
-burgerButton.addEventListener('click', () => {
-    menu.classList.toggle('is-active')
-})
 
+/* con la variable de ipad sirve de switch ya que nos servira para activar si el boton menu funciona con pantallas menores que el de 767px*/
+
+ipadResponse.addListener(validation)
+
+function validation (event) {
+    if(event.matches) {
+        /*metodo mas corto*/ 
+        burgerButton.addEventListener('click', () => {
+            menu.classList.toggle('is-active')
+        })
+        console.log('match')
+    }
+}
 /* con condicionales
 burgerButton.addEventListener('click', function (){
     if(menu.classList.contains('is-active') ) {
