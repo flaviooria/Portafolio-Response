8b0975327d7b3be21eafa1e6d25f92f41db1ced3
Flavio Oria <flavio.oriap@gmail.com>
2020-10-02 16:03:23 +0200

    se modifico el main js y el top del burger button

--- js/main.js
+++ js/main.js
@@ -3,25 +3,24 @@ const burgerButton = document.querySelector('#burger-menu')
 const ipadResponse= window.matchMedia('screen and (max-width:767px')
 
 
-/* ---- metodo mas corto 
 burgerButton.addEventListener('click', () => {
     menu.classList.toggle('is-active')
-}) ---- */ 
+}) 
 
 /*con la variable de ipad sirve de switch ya que nos servira para activar si el boton menu funciona con pantallas menores que el de 767px */
 
-ipadResponse.addListener(validation)
+// ipadResponse.addListener(validation)
 
-function validation (event) {
-    if(event.matches) {
-        burgerButton.addEventListener('click', () => {
-            menu.classList.toggle('is-active')
-        })
-        console.log('match')
-    }
-} 
+// function validation (event) {
+//     if(event.matches) {
+//         burgerButton.addEventListener('click', () => {
+//             menu.classList.toggle('is-active')
+//         })
+//         console.log('match')
+//     }
+// } 
 
-validation(ipadResponse)
+// validation(ipadResponse)
 
 
 /* con condicionales
