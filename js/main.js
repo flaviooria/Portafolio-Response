f1898fab068cb3d22833e9efe6d840395f416e75
Flavio Oria <flavio.oriap@gmail.com>
2020-10-02 15:28:44 +0200

    se hizo bug al error que teniamos con la funcion validation

--- js/main.js
+++ js/main.js
@@ -3,19 +3,27 @@ const burgerButton = document.querySelector('#burger-menu')
 const ipadResponse= window.matchMedia('screen and (max-width:767px')
 
 
-/* con la variable de ipad sirve de switch ya que nos servira para activar si el boton menu funciona con pantallas menores que el de 767px*/
+/* ---- metodo mas corto 
+burgerButton.addEventListener('click', () => {
+    menu.classList.toggle('is-active')
+}) ---- */ 
+
+/*con la variable de ipad sirve de switch ya que nos servira para activar si el boton menu funciona con pantallas menores que el de 767px */
 
 ipadResponse.addListener(validation)
 
 function validation (event) {
     if(event.matches) {
-        /*metodo mas corto*/ 
         burgerButton.addEventListener('click', () => {
             menu.classList.toggle('is-active')
         })
         console.log('match')
     }
-}
+} 
+
+validation(ipadResponse)
+
+
 /* con condicionales
 burgerButton.addEventListener('click', function (){
     if(menu.classList.contains('is-active') ) {
